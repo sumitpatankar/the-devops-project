298b93daa25d00cea37705a10a56f279f33ff874
sumitpatankar <sumitpatankar138@gmail.com>
2023-02-11 16:47:02 +0530

    Change aws provider

--- terraform/jenkins.tf
+++ terraform/jenkins.tf
@@ -1,12 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "4.54.0"
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
 }
 
+# Configure the AWS Provider
 provider "aws" {
   region = var.aws_region
 }
